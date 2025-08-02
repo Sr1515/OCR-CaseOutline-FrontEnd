f95a767d54d5b9f87b07e328ca97f5e00fd418ac
João Victor <joao151576@gmail.com>
2025-08-02 17:44:22 -0300

    api url changed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "dotenv/config";
-
 import { AuthProviderContext } from "./context/AuthProvider";
 import AppRoutes from "./routes/AppRoutes";
 
