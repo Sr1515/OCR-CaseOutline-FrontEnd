f28cf49a41915d781f5da10c09615feed7b083e2
João Victor <joao151576@gmail.com>
2025-08-02 16:53:49 -0300

    somes changed

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://ocr-caseoutline-backend-production.up.railway.app",
+  baseURL: "https://ocr-caseoutline-backend-production.up.railway.app",
   withCredentials: true,
 });
