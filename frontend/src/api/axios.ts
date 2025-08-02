a0e0f2ef253d763fb64c3f468360994aff079eca
João Victor <joao151576@gmail.com>
2025-08-02 17:37:10 -0300

    api url changed

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const api = axios.create({
   baseURL: process.env.VITE_API_URL,
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "dotenv/config";
 
-import dotenv from "dotenv";
-dotenv.config();
-
 import { AuthProviderContext } from "./context/AuthProvider";
 import AppRoutes from "./routes/AppRoutes";
 
